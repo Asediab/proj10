4af00614ffc56278d1273aa2491778d50045f312
Bohdan Koshman <asediaboli@gmail.com>
2020-07-25 19:42:02 +0200

    travis.yml

---
+++ docker/dev/init/db/docker-entrypoint-initdb.d/init-user-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "loan" --dbname "usr" <<-EOSQL
+    CREATE DATABASE document WITH OWNER loan;
+    CREATE DATABASE loan WITH OWNER loan;
+    CREATE DATABASE reservation WITH OWNER loan;
+EOSQL
