8380fa346ad8549c8918ebcef66d27191fd31995
Bohdan Koshman <asediaboli@gmail.com>
2020-08-12 09:26:06 +0200

    Bug Fix UserTest

--- microservice-user/src/test/java/com/biblio/microserviceuser/web/service/impl/UserServiceImpl_ITest.java
+++ microservice-user/src/test/java/com/biblio/microserviceuser/web/service/impl/UserServiceImpl_ITest.java
@@ -24,7 +24,7 @@ class UserServiceImpl_ITest {
      void start() {
         user.setId(1L);
         user.setSurname("User");
-        user.setEmail("ocr.proj7@yandex.ru");
+        user.setEmail("ocr.proj07@yandex.ru");
         user.setName("User");
         user.setPassword("$2a$10$a3w89n.b/aRcUYPwdPlO8.89WhanqcUYmDssnr0BgIQ98cK9bxN0q");
         user.setRoles(Collections.singleton(Role.USER));
@@ -35,7 +35,7 @@ class UserServiceImpl_ITest {
     @DisplayName("Test findByEmail")
     void findByEmail() {
         try {
-            User user1 = userServiceImpl.findByEmail("ocr.proj7@yandex.ru");
+            User user1 = userServiceImpl.findByEmail("ocr.proj07@yandex.ru");
             Assertions.assertNotNull(user1);
         } catch (Exception exception) {
             exception.printStackTrace();
